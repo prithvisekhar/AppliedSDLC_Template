a87405186eddd872cb6d8c549c35fe94b640583f
prithvisekhar <1872804+prithvisekhar@users.noreply.github.com>
2021-03-10 13:44:13 +0530

    Update test_calculator_operations.c

--- 3_Implementation/test/test_calculator_operations.c
+++ 3_Implementation/test/test_calculator_operations.c
@@ -38,29 +38,31 @@ int main()
 void test_add(void) {
   TEST_ASSERT_EQUAL(30, add(10, 20));
   TEST_ASSERT_EQUAL(-10, add(10, -20));
+  TEST_ASSERT_EQUAL(-30, add(-10, -20));
+  TEST_ASSERT_EQUAL(10, add(-10, 20));
 }
 void test_add_testcase2(void) {
  
   /* Dummy fail*/
-  TEST_ASSERT_EQUAL(1500, add(750, 7500));
+  // TEST_ASSERT_EQUAL(1500, add(750, 7500));
 }
 void test_subtract(void) {
   TEST_ASSERT_EQUAL(-3, subtract(0, 3));
   
   /* Dummy fail*/
-  TEST_ASSERT_EQUAL(1, subtract(1000, 900));
+  // TEST_ASSERT_EQUAL(1, subtract(1000, 900));
 }
 
 void test_multiply(void) {
   TEST_ASSERT_EQUAL(0, multiply(1, 0));
   
   /* Dummy fail*/
-  TEST_ASSERT_EQUAL(2, multiply(2, 5));
+  // TEST_ASSERT_EQUAL(2, multiply(2, 5));
 }
 
 void test_divide(void) {
   TEST_ASSERT_EQUAL(0, divide(1, 0));
   
   /* Dummy fail*/
-  TEST_ASSERT_EQUAL(3, divide(2, 2));
+  // TEST_ASSERT_EQUAL(3, divide(2, 2));
 }
